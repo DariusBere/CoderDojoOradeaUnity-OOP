44f440d5b23012fe79aa17f275519d01a225ea34
DariusBere <dariusbere8@gmail.com>
2015-10-25 15:28:02 +0200

    this is the first commit

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+    public Color color;
+    public float combustibil;
+    public float VitezaMaxima;
+    public float greutatea;
+    public float inaltimea;
+    public float pret;
+    public float CapacitateaBateriei;
+    public int Usi;
+    private float viteza = 0.0f ;
+
+	// Use this for initialization
+	void Deplasare () {
+        transform.position = transform.position + Vector3.forward * Time.deltaTime*viteza;
+	}
+	void Accelereaza () {
+        viteza = viteza + 0.1f; 
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Deplasare();
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Accelereaza();
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            viteza = 0.0f;
+        }
+	}
+}
